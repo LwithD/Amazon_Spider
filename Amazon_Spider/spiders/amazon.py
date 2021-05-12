153e905443784f3fb5cdf2b35dc53a0ba37198f4
bigface <904919863@qq.com>
2021-05-12 17:05:49 +0800

    修改为RedisCrawlSpider，更新了一下数据解析（name的解析好像还有点问题）

--- Amazon_Spider/spiders/amazon.py
+++ Amazon_Spider/spiders/amazon.py
@@ -2,20 +2,22 @@ import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from Amazon_Spider.items import AmazonSpiderItem
+from scrapy_redis.spiders import RedisCrawlSpider
 import re
 
-class AmazonSpider(CrawlSpider):
+class AmazonSpider(RedisCrawlSpider):
     name = 'amazon'
     # allowed_domains = ['https://www.amazon.com/',
     #         'https://aax-us-east.amazon-adsystem.com/']
-    start_urls = ['https://www.amazon.com/s?bbn=283155&rh=n%3A283155%2Cp_72%3A1250224011&dc&qid=1620543950&rnid=1250219011&ref=lp_1000_nr_p_72_3']
+    # start_urls = ['https://www.amazon.com/s?bbn=283155&rh=n%3A283155%2Cp_72%3A1250224011&dc&qid=1620543950&rnid=1250219011&ref=lp_1000_nr_p_72_3']
 
+    redis_key = 'amazon'
 
     link_list = LinkExtractor(allow = r'ref=sr_pg_\d+')
-    link_detail = LinkExtractor(allow=r'ref=sr_1_2')#&ref=sr_\d+_\d+
+    link_detail = LinkExtractor(allow=r'ref=sr_\d+_\d+')#&ref=sr_\d+_\d+
 
     rules = (
-        Rule(link_list,follow=False),#callback = 'parsesss',
+        Rule(link_list,follow=True),#callback = 'parsesss',
         Rule(link_detail, callback='parse_item', follow=False),
     )
 
@@ -26,10 +28,14 @@ class AmazonSpider(CrawlSpider):
 
     def parse_item(self, response):
         item = AmazonSpiderItem()
-        item['name'] = response.xpath('//*[@id="productTitle"]/text()').extract_first()
+        name = response.xpath('//*[@id="productTitle"]/text()').extract_first()
+        if isinstance(name, list):
+            name = ''.join(name)
+        name = name.replace('\n', '')
+        item['name'] = name
         item['author'] = response.xpath('//*[@id="bylineInfo"]/span/span[1]/a[1]/text()').extract_first()
         rating = response.xpath('//*[@id="acrPopover"]/span[1]/a/i[1]/span/text()').extract_first()
-        rating = re.findall('(\d+) out of',rating)[0]
+        rating = re.findall('(\d?\.?\d+) out of',rating)[0]
         item['rating'] = rating
         rating_num_response= response.xpath('//*[@id="acrCustomerReviewText"]/text()').extract_first()
         if ',' in rating_num_response:
@@ -39,15 +45,15 @@ class AmazonSpider(CrawlSpider):
         item['rating_num'] = rating_num
         li_list = response.xpath('//*[@id="tmmSwatches"]/ul/li')
         for li in li_list:     #/span/span[1]/span/a/span[2]/text()
-            price = li.xpath('./span/span[1]/span[1]/a/span[2]//text()').extract_first()
-            print(price)
+            price = li.xpath('./span/span[1]/span[1]/a/span[2]//text()').extract()
+            price = ''.join(price)
             price = re.findall('\$(\d+\.?\d+)',price)
             if price == []:
                 continue
             else:
-                price = price[0]
+                price = float(price[0])
             s = li.xpath('./span/span/span/a/span[1]/text()').extract_first()
-            if 'Kindle' in s:
+            if 'Kindle' in s or 'kindle' in s:
                 item['price_kindle']=price
             elif 'Paperback' in s:
                 item['price_paper'] = price
@@ -55,5 +61,5 @@ class AmazonSpider(CrawlSpider):
                 item['price_hard'] = price
             elif 'Audio CD' in s:
                 item['price_audioCD'] = price
-        # print(item)
+        print(item)
         yield item
